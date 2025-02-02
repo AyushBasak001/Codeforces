1fb2f2ac8cb41fad6b29480d694626ba45f6e413
AyushBasak001 <ayushbasak31@gmail.com>
2025-02-02 23:55:13 +0530

    Problem 41A

---
+++ Translation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s, t ;
+    cin>>s>>t ;
+
+    if(s.length()!=t.length())
+    {
+        cout<<"NO" ;
+        return 0 ;
+    }
+
+    for(int i=0 ; i<s.length() ; i++)
+    {
+        if(s[i]!=t[t.length()-1-i])
+        {
+            cout<<"NO" ;
+            return 0 ;
+        }
+    }
+
+    cout<<"YES" ;
+    return 0 ;
+}
